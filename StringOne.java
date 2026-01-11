370a6613654a2c8253e0d7f082f855dfe3c7632c
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-11 23:14:27 +0530

    strings

--- StringOne.java
+++ StringOne.java
@@ -1,10 +1,15 @@
 /*
-Strings 
+Q1. Write a Java program to print a given string.
+ Input : Hello Java
+ Outputt : Hello Java
 */
 import java.util.*;
 public class StringOne{
 	public static void main(String...x){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String");
+		String s= sc.nextLine();
+		System.out.println(s);
+		
 	}
 }
