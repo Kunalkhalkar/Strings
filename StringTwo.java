fdfb96a873cd2def6688c307ea353803a2d1e234
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-11 23:19:43 +0530

    strings

--- StringTwo.java
+++ StringTwo.java
@@ -6,6 +6,12 @@ public class StringTwo{
 	public static void main(String...x){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String");
+		String s = sc.nextLine();
+		int count =0;
+		for(int i = 0; i<s.length(); i++){
+			count++;
+		}
+		System.out.println("The length of string '"+s +"' is : "+ count);
 	}
 	
 }
