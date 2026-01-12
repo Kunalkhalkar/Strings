6c7d739290653d2ed864ec1baee4d313024a0647
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-12 22:31:33 +0530

    reverse

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+/*
+Q6. Write a program to reverse a given string.
+ Input : Java
+ Output : avaJ
+*/
+import java.util.*;
+public class Reverse{
+	public static void main(String ...x){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a string");
+		String s = sc.nextLine();
+		String s1 ="";	
+		for(int i= s.length()-1; i>=0; i--){
+			s1.concat(); 
+		}
+		System.out.println(s1);
+	}
+}
