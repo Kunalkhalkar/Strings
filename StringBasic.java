8675c1c06e572215e47412dfdbfc27d70c5f1065
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-12 08:31:34 +0530

    String

---
+++ StringBasic.java
@@ -0,0 +1,21 @@
+/*
+Q3. Write a program to count total characters in a string (excluding spaces).
+ Input : Java Language
+ Output : 12
+*/
+import java.util.*;
+public class StringBasic{
+	public static void main(String ...x){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the string");
+	String s = sc.nextLine();
+	int count = 0;
+	for(int i=0; i<s.length(); i++){
+		if(s.charAt(i) == ' '){
+			continue;
+		}
+		++count;
+	}
+	System.out.println(count);
+	}
+}
