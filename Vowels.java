8ce69accc64d4ecbf35536c7d3644f01c903d502
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-12 22:32:08 +0530

    vowels

---
+++ Vowels.java
@@ -0,0 +1,21 @@
+/*
+Q4. Write a program to count the number of vowels in a string.
+ Input : Education
+ Output : 5
+ */
+ import java.util.*;
+ public class Vowels{
+	 public static void main(String ...x){
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter a string");
+		 String s = sc.nextLine();
+		 
+		 int count=0;
+		 for(int i =0; i<s.length(); i++){
+			 if(s.charAt(i) == 'a'||s.charAt(i) == 'e'|| s.charAt(i) == 'i'||s.charAt(i) =='o'||s.charAt(i) =='u'){
+				 ++count;
+			 }
+		 }
+		 System.out.println(count);
+	 }
+ }
