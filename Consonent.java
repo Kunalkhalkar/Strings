f9740c0af30582d17a5b01f58589f24780fcadc3
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-13 23:25:09 +0530

    temp

--- Consonent.java
+++ Consonent.java
@@ -11,6 +11,7 @@ public class Consonent{
 		String s = sc.nextLine();
 		int count= 0;
 		for(int i= 0; i<s.length(); i++){
+			
 			if(s.charAt(i) !='a' && s.charAt(i) !='e' && s.charAt(i) !='i' && s.charAt(i) !='o' && s.charAt(i) !='u' )
 			{
 				++count;
