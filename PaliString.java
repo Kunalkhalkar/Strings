32fb0a9a9a6108e687ac47ccc8ccebd2b4ea2b9b
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-14 10:34:36 +0530

    pal

---
+++ PaliString.java
@@ -0,0 +1,13 @@
+/*
+Q7. Write a program to check whether a string is palindrome or not.
+ Input : madam
+ Output : Palindrome
+*/
+import java.util.*;
+public class PaliString.java{
+	public static void main(String...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String str = sc.next();
+	}
+}
