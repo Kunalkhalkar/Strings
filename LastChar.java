ef8d20001118438226925c132a333c667b18b4ed
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-16 00:32:12 +0530

    String

--- LastChar.java
+++ LastChar.java
@@ -1,14 +1,13 @@
-/*Q11. Write a program to print the first character of a string.
- Input : Apple
- Output : A
+/*Q12. Write a program to print the last character of a string.
+ Input : India
+ Output : a
 */
 import java.util.*;
-public class PrintString{
+public class LastChar{
 	public static void main(String ...x){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String");
 		String str = sc.next();
-		System.out.println(str.charAt(0));
-		
+		System.out.println(str.charAt(str.length()-1));
 	}
 }
