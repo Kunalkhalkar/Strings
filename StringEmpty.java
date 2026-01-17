054ba5be5b1cc510456505a18cdb4793cc829f4f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-17 22:57:37 +0530

    string

--- StringEmpty.java
+++ StringEmpty.java
@@ -8,8 +8,12 @@ public class StringEmpty{
 	public static void main(String ...x){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a String");
-		String str =sc.next();
-		
-		
+		String str = sc.next();
+		if(str.length() < 1){
+			System.out.println("String is Empty");
+		}
+		else{
+			System.out.println("String is not Empty");
+		}
 	}
 }
