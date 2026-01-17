6797b225e3a7a3ecea734d269e97426128ef4365
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-17 21:20:08 +0530

    String

---
+++ StringEmpty.java
@@ -0,0 +1,15 @@
+/*
+Q14. Write a program to check whether a string is empty or not.
+ Input : " "
+ Output : String is empty
+*/
+import java.util.*;
+public class StringEmpty{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String");
+		String str =sc.next();
+		
+		
+	}
+}
