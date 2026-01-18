895704721c42f0a45441f40e16b2931575db707b
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-18 23:17:32 +0530

    digits

--- CountDigits.java
+++ CountDigits.java
@@ -4,5 +4,16 @@
 */
 import java.util.*;
 public class CountDigits{
-
+	public static void main(String ...x){
+		int count=0;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a string");
+		String s = sc.nextLine();
+		for(int i=0; i<s.length(); i++){
+			if((int)(s.charAt(i)) >47 && (int)(s.charAt(i)) < 58){
+				++count;
+			}
+		}
+		System.out.println("the digits Present in String is : "+count);
+	}
 }
