16f51861db81c58b9368a03b1eb21b9da17ed577
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-18 23:17:49 +0530

    msg

---
+++ RemoveSpaces.java
@@ -0,0 +1,13 @@
+/*
+Q16. Write a program to remove all spaces from a string.
+ Input : Java Programming
+ Output : JavaProgramming
+*/
+import java.util.*;
+public class RemoveSpaces{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String str = sc.nextLine();
+	}
+}
