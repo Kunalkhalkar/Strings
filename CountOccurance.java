12546db802651d267b53c0407022b10413a2fa37
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-19 22:42:48 +0530

    count

--- CountOccurance.java
+++ CountOccurance.java
@@ -1,2 +1,16 @@
 /*
+Q19. Write a program to count occurrence of a given character in a string.
+  Input: programming, 
+ 	Character→ g
+  Output: 2
 */
+import java.util.*;
+public class CountOccurance{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String.");
+		String str= sc.nextLine();
+		System.out.println("Enter the character to find occurance count");
+		char c = sc.next().charAt(0);
+	}
+}
