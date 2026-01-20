5233cb5c56bce3b882bb1282bbec70693bcca28e
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-20 11:15:00 +0530

    sub String

---
+++ SubString.java
@@ -0,0 +1,13 @@
+/*Q20. Write a program to check whether a string contains a substring.
+ Input: Java Programming, 
+Java
+ Output : Substring found
+*/
+import java.util.*;
+public class SubString{
+	public static void main(String...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String str = sc.nextLine();
+	}
+}
