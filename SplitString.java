9f151800657a79e8398c60f3fb235e5552db9ab5
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-21 13:22:09 +0530

    string

---
+++ SplitString.java
@@ -0,0 +1,11 @@
+/*Q23. Split the string into words and find the word with maximum length.
+ Input : Java is very powerful
+ Output : powerful
+*/
+import java.util.*;
+public class SplitString{
+	public static void main(String ...x){
+		Scanner sc = new ScannerI(System.in);
+		System.out.println("Enter the String");
+	}
+}
