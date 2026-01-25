38e1960c1544b24f15e56295c6e32e10b581c8e5
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-25 22:26:59 +0530

    String

--- SplitString.java
+++ SplitString.java
@@ -3,27 +3,35 @@
  Output : powerful
 */
 import java.util.*;
-public class SplitString{
-	public static void main(String ...x){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the String");
-		String str = sc.nextLine();
-		StringBuilder sn = new StringBuilder();
-		int count = 0;
-		for(int i = 0; i < str.length(); i++){
-		StringBuilder sb = new StringBuilder("");
-		count = 0;
-			if(str.charAt(i) != ' '){
-				sb.append(str.charAt(i));				
-			}
-			
-			else{
-				if(sb.length() > count){
-					count = sb.length();
-					sn = sb;
-				}
-			}
-		}
-		System.out.println("String is : "+sn+" Length is : "+count);
-	}
+
+public class SplitString {
+    public static void main(String ...x) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str = sc.nextLine();
+
+        StringBuilder currentWord = new StringBuilder();
+        String longestWord = "";
+        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            
+            if (ch != ' ') {
+                currentWord.append(ch);
+            } else {
+                if (currentWord.length() > longestWord.length()) {
+                    longestWord = currentWord.toString();
+                }
+                currentWord.setLength(0); // reset for next word
+            }
+        }
+
+        // check last word
+        if (currentWord.length() > longestWord.length()) {
+            longestWord = currentWord.toString();
+        }
+
+        System.out.println("Word with maximum length: " + longestWord);
+        System.out.println("Length is: " + longestWord.length());
+    }
 }
